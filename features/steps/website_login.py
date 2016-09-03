d4d12f335c601906f8a99d3a9396ca811a062222
layheng <henglay@hotmail.com>
2016-09-03 03:07:23 -0400

    update login

--- features/steps/website_login.py
+++ features/steps/website_login.py
@@ -7,23 +7,24 @@ from hamcrest import *
 def step_impl(context):
     url = "https://" + context.server_ip
     context.driver.get(url)
+    context.title_logout = context.driver.title
 
 
 @when(u'login with user element {user} and password element {password}')
 def step_impl(context, user, password):
-    username = context.driver.find_element_by_id(user)
-    password = context.driver.find_element_by_id(password)
-    username.send_keys(context.username)
-    password.send_keys(context.password)
+    driver_username = context.driver.find_element_by_id(user)
+    driver_password = context.driver.find_element_by_id(password)
+    driver_username.send_keys(context.username)
+    driver_password.send_keys(context.password)
 
 
 @when(u'submit element {login}')
 def step_impl(context, login):
-    login = context.driver.find_element_by_id(login)
-    login.submit()
+    driver_login = context.driver.find_element_by_id(login)
+    driver_login.submit()
 
 
 @then(u'login is success')
 def step_impl(context):
-    pass
-
+    title_login = context.driver.title
+    assert_that(title_login, is_not(context.title_logout))
