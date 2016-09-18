b3d65811c95a2bd4519812a9587900c311890519
layheng <henglay@hotmail.com>
2016-09-18 00:39:07 -0400

    update icon and test view

--- tester/views.py
+++ tester/views.py
@@ -11,6 +11,7 @@ def index(request):
     args = shlex.split(command_line)
     (out, err) = Popen(args, stdout=PIPE, stderr=PIPE).communicate()
     feature_list_str = out.splitlines()
+
     # update database for feature list
     Feature.objects.all().delete()
     for line in feature_list_str:
@@ -18,6 +19,7 @@ def index(request):
         feature = Feature(name=file_name, file_path=line)
         feature.save()
     feature_list = Feature.objects.order_by('name')
+
     # extract user data
     user_data = []
     start_user_data = False
@@ -29,6 +31,7 @@ def index(request):
                 user_data.append(line)
             elif "end of userdata" in line:
                 break
+
     # update database for user data
     UserData.objects.all().delete()
     for line in user_data:
@@ -36,6 +39,7 @@ def index(request):
         user_data = UserData(name=temp_list[0], value=temp_list[1].strip())
         user_data.save()
     user_data_list = UserData.objects.all()
+
     context = {'feature_list': feature_list, 'user_data_list': user_data_list}
     return render(request, 'tester/index.html', context)
 
@@ -54,62 +58,23 @@ def result(request):
         for line in f:
             result_lines.append(line)
 
-    # read feature/scenario/steps passed/failed line
-    feature_list = result_report_str[last_four_lines].split()
-    total_features = int(feature_list[0]) + int(feature_list[3])
-    if total_features > 0:
-        feature_pass = int(feature_list[0]) * 100 / total_features
-    else:
-        feature_pass = 0
-    feature_fail = 100 - feature_pass
-    feature_passed_percentage = str(feature_pass) + "%"
-    feature_failed_percentage = str(feature_fail) + "%"
-    feature_passed_progress = "width: " + feature_passed_percentage
-    feature_failed_progress = "width: " + feature_failed_percentage
+    # Decode feature/scenario/steps passed/failed numbers
+    (passed_percentage, failed_percentage,
+     passed_progress, failed_progress) = decode_test_summary(result_summary)
 
-    scenario_list = result_report_str[last_four_lines+1].split()
-    total_scenario = int(scenario_list[0]) + int(scenario_list[3])
-    if total_scenario > 0:
-        scenario_pass = int(scenario_list[0]) * 100 / total_scenario
-    else:
-        scenario_pass = 0
-    scenario_fail = 100 - scenario_pass
-    scenario_passed_percentage = str(scenario_pass) + "%"
-    scenario_failed_percentage = str(scenario_fail) + "%"
-    scenario_passed_progress = "width: " + scenario_passed_percentage
-    scenario_failed_progress = "width: " + scenario_failed_percentage
-
-    step_list = result_report_str[last_four_lines+2].split()
-    total_steps = int(step_list[0]) + int(step_list[3])
-    if total_steps > 0:
-        step_pass = int(step_list[0]) * 100 / total_steps
-    else:
-        step_pass = 0
-    step_fail = 100 - step_pass
-    step_passed_percentage = str(step_pass) + "%"
-    step_failed_percentage = str(step_fail) + "%"
-    step_passed_progress = "width: " + step_passed_percentage
-    step_failed_progress = "width: " + step_failed_percentage
-
-    context = {'result_lines': result_lines, 'result_summary': result_summary,
-               'feature_passed_progress': feature_passed_progress,
-               'feature_failed_progress': feature_failed_progress,
-               'feature_passed_percentage': feature_passed_percentage,
-               'feature_failed_percentage': feature_failed_percentage,
-               'scenario_passed_progress': scenario_passed_progress,
-               'scenario_failed_progress': scenario_failed_progress,
-               'scenario_passed_percentage': scenario_passed_percentage,
-               'scenario_failed_percentage': scenario_failed_percentage,
-               'step_passed_progress': step_passed_progress,
-               'step_failed_progress': step_failed_progress,
-               'step_passed_percentage': step_passed_percentage,
-               'step_failed_percentage': step_failed_percentage}
+    context = {'result_lines': result_lines,
+               'result_summary': result_summary,
+               'passed_progress': passed_progress,
+               'failed_progress': failed_progress,
+               'passed_percentage': passed_percentage,
+               'failed_percentage': failed_percentage}
     return render(request, 'tester/result.html', context)
 
 
 def detail(request, feature_id):
     feature = get_object_or_404(Feature, pk=feature_id)
     feature.scenario_set.all().delete()
+
     # update scenarios to database for a selected feature
     previous_line = ''
     with open(os.path.join(feature.file_path)) as file:
@@ -123,6 +88,7 @@ def detail(request, feature_id):
                 scenario.save()
             else:
                 previous_line = line
+
     context = {'feature': feature}
     return render(request, 'tester/detail.html', context)
 
@@ -152,54 +118,72 @@ def detailresult(request, feature_id):
         for line in f:
             result_lines.append(line)
 
-    # read feature/scenario/steps passed/failed line
-    feature_list = result_report_str[last_four_lines].split()
-    total_features = int(feature_list[0]) + int(feature_list[3])
-    if total_features > 0:
-        feature_pass = int(feature_list[0]) * 100 / total_features
-    else:
-        feature_pass = 0
-    feature_fail = 100 - feature_pass
-    feature_passed_percentage = str(feature_pass) + "%"
-    feature_failed_percentage = str(feature_fail) + "%"
-    feature_passed_progress = "width: " + feature_passed_percentage
-    feature_failed_progress = "width: " + feature_failed_percentage
+    # Decode feature/scenario/steps passed/failed numbers
+    (passed_percentage, failed_percentage,
+     passed_progress, failed_progress) = decode_test_summary(result_summary)
 
-    scenario_list = result_report_str[last_four_lines + 1].split()
-    total_scenario = int(scenario_list[0]) + int(scenario_list[3])
-    if total_scenario > 0:
-        scenario_pass = int(scenario_list[0]) * 100 / total_scenario
-    else:
-        scenario_pass = 0
-    scenario_fail = 100 - scenario_pass
-    scenario_passed_percentage = str(scenario_pass) + "%"
-    scenario_failed_percentage = str(scenario_fail) + "%"
-    scenario_passed_progress = "width: " + scenario_passed_percentage
-    scenario_failed_progress = "width: " + scenario_failed_percentage
-
-    step_list = result_report_str[last_four_lines + 2].split()
-    total_steps = int(step_list[0]) + int(step_list[3])
-    if total_steps > 0:
-        step_pass = int(step_list[0]) * 100 / total_steps
-    else:
-        step_pass = 0
-    step_fail = 100 - step_pass
-    step_passed_percentage = str(step_pass) + "%"
-    step_failed_percentage = str(step_fail) + "%"
-    step_passed_progress = "width: " + step_passed_percentage
-    step_failed_progress = "width: " + step_failed_percentage
-
-    context = {'result_lines': result_lines, 'result_summary': result_summary,
-               'feature_passed_progress': feature_passed_progress,
-               'feature_failed_progress': feature_failed_progress,
-               'feature_passed_percentage': feature_passed_percentage,
-               'feature_failed_percentage': feature_failed_percentage,
-               'scenario_passed_progress': scenario_passed_progress,
-               'scenario_failed_progress': scenario_failed_progress,
-               'scenario_passed_percentage': scenario_passed_percentage,
-               'scenario_failed_percentage': scenario_failed_percentage,
-               'step_passed_progress': step_passed_progress,
-               'step_failed_progress': step_failed_progress,
-               'step_passed_percentage': step_passed_percentage,
-               'step_failed_percentage': step_failed_percentage}
+    context = {'result_lines': result_lines,
+               'result_summary': result_summary,
+               'passed_progress': passed_progress,
+               'failed_progress': failed_progress,
+               'passed_percentage': passed_percentage,
+               'failed_percentage': failed_percentage}
     return render(request, 'tester/result.html', context)
+
+
+def decode_test_summary(summary):
+    """
+    Decode test result summary
+    :param summary: list of test summary with features, scenarios and steps
+    :return: passed/failed percentage and progress
+    """
+    passed_percentage = []
+    failed_percentage = []
+    passed_progress = []
+    failed_progress = []
+    pass_index = 0
+    fail_index = 3
+
+    # decode features
+    feature_list = summary[0].split()
+    total_features = int(feature_list[pass_index]) + int(feature_list[fail_index])
+    if total_features > 0:
+        pass_value = int(feature_list[pass_index]) * 100.0 / total_features
+    else:
+        pass_value = 0.0
+    feature_pass = "{0:.2f}".format(pass_value)
+    feature_fail = "{0:.2f}".format(100.0 - pass_value)
+    passed_percentage.append(feature_pass + "%")
+    failed_percentage.append(feature_fail + "%")
+    passed_progress.append("width: " + feature_pass + "%")
+    failed_progress.append("width: " + feature_fail + "%")
+
+    # decode scenarios
+    scenario_list = summary[1].split()
+    total_scenario = int(scenario_list[pass_index]) + int(scenario_list[fail_index])
+    if total_scenario > 0:
+        pass_value = int(scenario_list[pass_index]) * 100.0 / total_scenario
+    else:
+        pass_value = 0.0
+    scenario_pass = "{0:.2f}".format(pass_value)
+    scenario_fail = "{0:.2f}".format(100.0 - pass_value)
+    passed_percentage.append(scenario_pass + "%")
+    failed_percentage.append(scenario_fail + "%")
+    passed_progress.append("width: " + scenario_pass + "%")
+    failed_progress.append("width: " + scenario_fail + "%")
+
+    # decode steps
+    step_list = summary[2].split()
+    total_steps = int(step_list[pass_index]) + int(step_list[fail_index])
+    if total_steps > 0:
+        pass_value = int(step_list[pass_index]) * 100.0 / total_steps
+    else:
+        pass_value = 0.0
+    step_pass = "{0:.2f}".format(pass_value)
+    step_fail = "{0:.2f}".format(100.0 - pass_value)
+    passed_percentage.append(step_pass + "%")
+    failed_percentage.append(step_fail + "%")
+    passed_progress.append("width: " + step_pass + "%")
+    failed_progress.append("width: " + step_fail + "%")
+
+    return passed_percentage, failed_percentage, passed_progress, failed_progress
