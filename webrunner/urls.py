22ed1886c044223af03dc6233cd10cb38d00c93f
layheng <henglay@hotmail.com>
2016-09-21 00:42:32 -0400

    redirect home page

--- webrunner/urls.py
+++ webrunner/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    url(r'^$', lambda r: HttpResponseRedirect('tester/')),
     url(r'^tester/', include('tester.urls')),
     url(r'^admin/', admin.site.urls),
 ]
