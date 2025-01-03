90fae9be06eebe1702136839e680e6f46c2e4e42
layheng <henglay@hotmail.com>
2025-01-03 16:38:22 -0500

    Updated using the Selenium.Webdriver.find_element method

--- features/steps/facebook_login.py
+++ features/steps/facebook_login.py
@@ -3,6 +3,7 @@ from hamcrest import assert_that, is_not
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
 import time
 
 
@@ -16,8 +17,8 @@ def step_impl(context):
 
 @when(u'login with user element {user} and password element {password}')
 def step_impl(context, user, password):
-    driver_username = context.driver.find_element_by_id(user)
-    driver_password = context.driver.find_element_by_id(password)
+    driver_username = context.driver.find_element(By.ID, user)
+    driver_password = context.driver.find_element(By.ID, password)
     driver_username.send_keys(context.username)
     driver_password.send_keys(context.password)
     time.sleep(0.1)
@@ -26,7 +27,7 @@ def step_impl(context, user, password):
 
 @then(u'login is success')
 def step_impl(context):
-    time.sleep(2)
+    time.sleep(5)
     assert_that(context.driver.title, is_not(context.title_logout))
     time.sleep(0.1)
     context.driver.quit()
